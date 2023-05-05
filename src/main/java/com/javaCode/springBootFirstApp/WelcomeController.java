8c2404bae76f2f737f79a38de4333131d99d3ae7
hamza iqbal <i200856@nu.edu.pk>
2023-05-05 16:04:59 +0500

    added zainab's reality (2)

--- src/main/java/com/javaCode/springBootFirstApp/WelcomeController.java
+++ src/main/java/com/javaCode/springBootFirstApp/WelcomeController.java
@@ -8,6 +8,6 @@ public class WelcomeController {
 
     @GetMapping("/welcome")
     public String welcome(){
-        return "Welcome to spring boot world, by Hamza, And Zainab and zaraaaa and CHowi SAHB" ;
+        return "Welcome to spring boot world, by Hamza, And Zainab and zaraaaa and CHowi SAHB, Zainab i hate u" ;
     }
 }
